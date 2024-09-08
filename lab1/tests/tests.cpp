05e56b474a7af685387d18ae62835611722a0794
Sameman <no@email.com>
2024-09-08 15:50:44 +0000

    Fixed tests

--- lab1/tests/tests.cpp
+++ lab1/tests/tests.cpp
@@ -31,8 +31,3 @@ TEST(PolyndromTests, FalseCases) {
 
     EXPECT_FALSE(is_polyndrom("bbccba"));
 }
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-}
