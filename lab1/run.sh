55f7c59cb1c42934bd2e66be5b5048abfdaa814c
Sameman <no@email.com>
2024-09-18 15:52:20 +0000

    Added README and bash scripts

---
+++ lab1/run.sh
@@ -0,0 +1,4 @@
+pushd build
+chmod +x ./lab1
+./lab1
+popd
--- lab1/tests/tests.cpp
+++ lab1/tests/tests.cpp
@@ -3,7 +3,7 @@
 #include "../src/polindrom.h"
 
 
-TEST(PolyndromTests, TrueCases) {
+TEST(PolyndromTests, PositiveCases) {
     EXPECT_TRUE(is_polyndrom(""));
     EXPECT_TRUE(is_polyndrom("a"));
     EXPECT_TRUE(is_polyndrom("aa"));
@@ -15,19 +15,15 @@ TEST(PolyndromTests, TrueCases) {
     EXPECT_TRUE(is_polyndrom("abccba"));
 }
 
-TEST(PolyndromTests, FalseCases) {
+TEST(PolyndromTests, NegativeCases) {
     EXPECT_FALSE(is_polyndrom("ab"));
     EXPECT_FALSE(is_polyndrom("ba"));
-
     EXPECT_FALSE(is_polyndrom("bba"));
     EXPECT_FALSE(is_polyndrom("abb"));
     EXPECT_FALSE(is_polyndrom("aab"));
     EXPECT_FALSE(is_polyndrom("abc"));
-
     EXPECT_FALSE(is_polyndrom("abca"));
-
     EXPECT_FALSE(is_polyndrom("acbba"));
     EXPECT_FALSE(is_polyndrom("abbbc"));
-
     EXPECT_FALSE(is_polyndrom("bbccba"));
 }
